4eeea83bda493399458d9ecb2a23e5fa465e4f31
Nicolae Carabut <nicolae.carabut@gmail.com>
2016-04-07 09:29:36 -0700

    v1

---
+++ killflash.cmd
@@ -0,0 +1,2 @@
+takeown /F C:\Windows\WinSxS\wow64_adobe-flash-for-windows_31bf3856ad364e35_6.2.9200.20654_none_8f68461840b9d200 /R
+icacls C:\Windows\WinSxS\wow64_adobe-flash-for-windows_31bf3856ad364e35_6.2.9200.20654_none_8f68461840b9d200 /grant Everyone:F
